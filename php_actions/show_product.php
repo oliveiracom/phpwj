0bd6fb5b786d1f3c9e17701047652dae2d01c41a
oliveiracom <caroline@oliveiracomunicacao.com.br>
2020-05-23 16:19:00 -0300

    Read Phase

---
+++ php_actions/show_product.php
@@ -0,0 +1,19 @@
+<?php
+require('db_connect.php');
+
+$id = 2; // change to post value
+$sql = "SELECT * FROM products WHERE id = $id";
+
+$result = mysqli_query($connect, $sql);
+
+if ($result) {
+    $r = mysqli_fetch_assoc($result);
+
+    echo json_encode($r);
+
+  } else {
+    echo "Error: " . $sql . "<br>" . $connect->error;
+  }
+
+
+?>
